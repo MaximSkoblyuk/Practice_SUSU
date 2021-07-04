7c7b0a1e83d49d79f2ce5d9c9f76194808337dc1
MaximSkoblyuk <74621679+MaximSkoblyuk@users.noreply.github.com>
2021-07-04 16:07:53 +0500

    Commit input, output files

--- modules/input.py
+++ modules/input.py
@@ -1,6 +1,8 @@
 def input_file(file_name: str):
     file = open(file_name, 'r')
     input_str = file.read().strip()
+    file.close()
     return input_str
+
 
 # print(input_file('../files/input.txt'))
--- modules/output.py
+++ modules/output.py
@@ -1,1 +1,8 @@
 def output_file(file_name: str, answer: str):
+    file = open(file_name, 'w')
+    file.write(answer)
+    file.close()
+    return 0
+
+
+# output_file('../files/output.txt', 'ACCEPT')
